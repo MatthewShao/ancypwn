77640f42c6aaeca0a0dfcc7a6e2e2df67e436e4b
Matthew Shao <me@matshao.com>
2019-05-14 09:04:32 +0800

    update.

--- build.sh
+++ build.sh
@@ -32,7 +32,7 @@ ${sudo_cmd} docker build ancypwn-docker/16.04 --tag ancypwn:16.04
 
 # build 18.04
 echo "Building 18.10 docker image..."
-${sudo_cmd} docker build ancypwn-docker/18.04 --tag ancypwn:18.04
+#${sudo_cmd} docker build ancypwn-docker/18.04 --tag ancypwn:18.04
 
 # build 14.04 <-- this is not working yet..
 #${sudo_cmd} docker build ancypwn-docker/14.04 --tag ancypwn:14.04
